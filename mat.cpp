419265d62861f99a9d66e863d7d892da57b91f1d
israel_yaacobovich <77208779+Israel0545488592@users.noreply.github.com>
2022-03-14 14:43:31 +0200

    Add files via upload
    works on linux

--- mat.cpp
+++ mat.cpp
@@ -1,23 +1,23 @@
-#include "mat.h"
+#include "mat.hpp"
 
 matrix* createMat(int col, int row) {
 
     if (row < 1 || col < 1) { return NULL; }
 
     matrix* mat = new matrix;
-    if (!mat) { return NULL; }
+    if (mat == nullptr){ return NULL;}
 
     mat->col = col;
     mat->row = row;
     mat->start = new char[col * row];
-    if (!mat->start) { return NULL; }
+    if (mat -> start == nullptr) { return NULL; }
 
     return mat;
 }
 
 void releseMat(matrix* mat) {
 
-    delete[](mat->start);
+    delete[](mat -> start);
     delete mat;
 }
 
@@ -26,68 +26,76 @@ void releseMat(matrix* mat) {
 
 void carpet(matrix* mat, char ch1, char ch2) {
 
-    int iter = 0, count = 0, loop = 0;  //iterator, counter, indication to which layer we'r in
-    int col = mat->col;
-    int row = mat->row;
-    char ch = ch1, dir = 'r';   // r - right, l - left ext...
+    //setup
+    int iter = 0;   //iterator
+    int count = 0;  //counter
+    int loop = 0;   //indication in to how 'deep' we are in the matrix
+    int col = mat -> col;
+    int row = mat -> row;
+    char ch = ch1;  //wich charcter decorates current layer
 
-    while (count < col * row) {
+    while (count < col * row) {                  
 
-        switch (dir) {
+        for (int i = 0; i < col - 2*loop; i++) {        //right
 
-        case 'r':
-
-            for (int i = 0; (i < col - 2*loop) && count < col * row; i++) {
-
-                mat->start[iter] = ch;
-                iter++;
-                count++;
-            }
-            iter--;         //retret to the matrix'es bounds
-            iter += col;    //preper for next
-            dir = 'd';      //diraction
-
-        case 'd':
-
-            for (int i = 0; (i < row - 1 - 2*loop) && count < col * row; i++) {
-
-                mat->start[iter] = ch;
-                iter += col;
-                count++;
-            }
-            iter -= col;
-            iter--;
-            dir = 'l';
-
-        case 'l':
-
-            for (int i = 0; (i < col - 1 - 2*loop) && count < row * col; i++) {
-
-                mat->start[iter] = ch;
-                iter--;
-                count++;
-            }
+            mat->start[iter] = ch;
             iter++;
-            iter -= col;
-            dir = 'u';
-
-        case 'u':
-
-            for (int i = 0; (i < row -2 - 2*loop) && count < col * row; i++) {
-
-                mat->start[iter] = ch;
-                iter -= col;
-                count++;
-            }
-            iter += col;
-            iter++;
-            dir = 'r';
-
-        default:
-            break;
+            count++;
         }
+        iter--;         //retret to the matrix'es bounds
+        iter += col;
+
+        for (int i = 0; i < row - 1 - 2*loop; i++) {    //down
+
+            mat -> start[iter] = ch;
+            iter += col;
+            count++;
+        }
+        iter -= col;
+        iter--;
+
+        for (int i = 0; i < col - 1 - 2*loop; i++) {   //left
+
+            mat -> start[iter] = ch;
+            iter--;
+            count++;
+        }
+        iter++;
+        iter -= col;
+
+        for (int i = 0; i < row -2 - 2*loop; i++) {    //up
+
+            mat->start[iter] = ch;
+            iter -= col;
+            count++;
+        }
+        iter += col;
+        iter++;
 
         ch == ch1 ? ch = ch2 : ch = ch1;  //fliping charcter for new layer     
         loop++;
     }
+}
+
+string ariel::mat(int col, int row, char ch1, char ch2) {
+
+    string ans = "";
+
+    matrix* mat = createMat(col, row);
+
+    if (mat == nullptr) { return ans; }
+
+    carpet(mat, ch1, ch2);
+
+    for (int i = 0; i < mat->col; i++) {
+        for (int j = 0; j < mat->row; j++) {
+
+            ans += mat->start[i + (j * mat->col)];
+        }
+        ans += '\n';
+    }
+
+    releseMat(mat);
+
+    return ans;
 }
---
+++ mat.hpp
@@ -0,0 +1,25 @@
+#ifndef _MAT_HPP_
+#define _MAT_HPP_
+
+#include <string>
+
+using namespace std;
+
+typedef struct matrix {
+
+    //size
+    int row;
+    int col;
+
+    char* start; //the actuall data
+}matrix;
+
+matrix* createMat(int, int);
+
+void carpet(matrix*, char, char); //to ad the info as required 
+
+void releseMat(matrix*); //free memory
+
+namespace ariel { string mat(int, int, char, char); } //output function
+
+#endif
